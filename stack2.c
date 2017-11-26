cda18eeabb13fa7c708b3bdfa9677723abd9c05d
Hovsep Vardanyan <33977944+VHovsep@users.noreply.github.com>
2017-11-26 11:29:09 +0400

    Update stack2.c

--- stack2.c
+++ stack2.c
@@ -1,1 +1,23 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
+int main(int argc, char **argv)
+{
+  volatile int modified;
+  char buffer[64];
+
+  modified = 0;
+  int a = 10;
+  int b = 20;
+  if (a > 10) {
+    return 0;
+  }
+  gets(buffer);
+
+  if(modified != 0) {
+      printf("you have changed the 'modified' variable\n");
+  } else {
+      printf("Try again?\n");
+  }
+}
